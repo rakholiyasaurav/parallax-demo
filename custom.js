e693e52bf7cf830d3a6edc701d24da14001b4826
Saurav Rakholiya <saurav@hulkapps.com>
2021-08-12 12:01:41 +0530

    parallax demo

---
+++ custom.js
@@ -0,0 +1,3 @@
+$('.parallax-window').parallax({imageSrc: 'images/natural.jpg'});
+$('.parallax-window1').parallax({imageSrc: 'images/baloon.jpg'})
+$('.parallax-window2').parallax({imageSrc: 'images/sunset.jpg'})
